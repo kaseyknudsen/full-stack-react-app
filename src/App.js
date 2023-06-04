360c82100e2f344f5a36499010e6a87f010c1401
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-06-03 19:43:18 -0700

    deleted unused files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState("This is your data");
 
   useEffect(() => {
     fetch("/api")
@@ -13,10 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
